5a59657c2fffc88ffea7e657e22ac72935cd7b5d
Kane <69764836+kanelee1995@users.noreply.github.com>
2022-03-08 01:01:09 +0800

    hashrotuer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 // import Routes from "./Routes";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter
+    <HashRouter
     basename="/">
       <App />
-    </BrowserRouter >
+    </HashRouter >
   </React.StrictMode>,
   document.getElementById("root")
 );
