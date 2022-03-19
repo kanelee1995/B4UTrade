be802383f79b869acd5307d27f289d3fdb1bdd75
Kane <69764836+kanelee1995@users.noreply.github.com>
2022-03-19 21:34:44 +0800

    added TDD learning file

---
+++ src/Component/Test.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const Test = () => {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+};
+
+export default Test;
