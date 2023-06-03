fccc2bbe27993e396fc810dc45fc26b1a720f4f6
Kane <69764836+kanelee1995@users.noreply.github.com>
2023-06-03 08:40:48 -0400

    Ceated back up for apps

---
+++ App.backup.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import StockDetail from "./Route/StockDetail";
+import MainContent from "./Route/MainContent";
+import Header from "./Route/Header";
+import "./Assets/Styles/normalize.css";
+import "./Assets/Styles/style.css";
+
+function App() {
+  const [userInput, setuserInput] = useState("AAPL");
+  const inputStorage = localStorage.getItem("userInput");
+  const inputHandler = (e) => {
+    setuserInput(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    localStorage.setItem("userInput", userInput.toUpperCase());
+    document.getElementById("searchButton").click();
+    window.location.reload();
+  };
+
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+
+        <Switch>
+          {/* Route - Home */}
+          <Route exact path="/">
+            <MainContent
+              userInput={userInput}
+              inputHandler={inputHandler}
+              handleSubmit={handleSubmit}
+            />
+          </Route>
+          {/* Route - Stock details page */}
+          <Route path="/stockdetail">
+            <StockDetail userInput={inputStorage} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
