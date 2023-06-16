fa068723d1e4b933ffb418432a8f1ccb2be69fe0
Kane <69764836+kanelee1995@users.noreply.github.com>
2023-06-15 22:04:38 -0400

    improved code

--- src/Component/IndexBar.js
+++ src/Component/IndexBar.js
@@ -1,22 +1,44 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const IndexBar = ({ datas }) => {
-  const slicedDatas = datas.slice(0, 3);
+const IndexBar = () => {
+  const [mostGainerData, setMostGainerData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://financialmodelingprep.com/api/v3/gainers?apikey=d7f8484c1c8ac4235b39e22345b8dbbd")
+      .then((response) => {
+        setMostGainerData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching gainer data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  const slicedDatas = mostGainerData.slice(0, 3);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <header className="indexBar">
       <div className="indexList">
         {slicedDatas.map((data) => (
-          <div className="indexBarItem" key={data["ticker"]}>
-            {data["ticker"]}
+          <div className="indexBarItem" key={data.ticker}>
+            {data.ticker}
             <span className="whiteSpace">
-              {Math.round(data["changesPercentage"] * 100) / 100}%
+              {Math.round(data.changesPercentage * 100) / 100}%
               <span className="whiteSpace">
                 <FontAwesomeIcon icon={faAngleUp} />
               </span>
             </span>
-            <p className="indexCompanyName">{data["companyName"]}</p>
+            <p className="indexCompanyName">{data.companyName}</p>
           </div>
         ))}
       </div>
