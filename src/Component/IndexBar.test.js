1dca8039ba6db2b7f49dfe7326aaf186bb5bcd83
Kane <69764836+kanelee1995@users.noreply.github.com>
2023-08-03 22:42:08 -0400

    added comment for reference

--- src/Component/IndexBar.test.js
+++ src/Component/IndexBar.test.js
@@ -11,11 +11,13 @@ jest.mock("axios");
 describe("Index Bar successfully fectched", () => {
   beforeEach(() => {
     const response = "Hello World";
+    // set up the mock axios before render, then the axio will replace the true axios in the component (call with arguemnt in the component)
     axios.get.mockResolvedValue(response);
     render(<IndexBar />);
   });
 
   it("should fetches API when page load", async () => {
+    // if don't waitFor, it will run the test immediately (call stack) while the promise is still in the callback queue
     await waitFor(() => expect(axios.get).toHaveBeenCalled());
   });
 
@@ -32,14 +34,13 @@ describe("Index Bar successfully fectched", () => {
 
 
 it("should show loading when fetching", () => {
+    // set up a promise which stays in the callback queue
     axios.get.mockImplementation(() => new Promise(() => {}));
     render(<IndexBar />);
-
     expect(screen.getByTestId("loader")).toBeInTheDocument();
   });
 
 it("should display error message when fetch failed", async () => {
-//   axios.get.mockRejectedValue("error!");
     act(() => {
         axios.get.mockImplementation(() => new Promise((resolve, reject) => {
             reject("error!")
